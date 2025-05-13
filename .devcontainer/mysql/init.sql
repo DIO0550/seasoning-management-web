aff219b8e7236b95d26d43ffcf52a01cf5a65aa1
DIO0550 <diot12345@yahoo.co.jp>
2025-05-13 22:55:12 +0900

    ✨ [New Feature]: Add devcontainer configuration for Node.js and MySQL

---
+++ .devcontainer/mysql/init.sql
@@ -0,0 +1,13 @@
+-- Initialization script for the database
+-- This script will be executed when the MySQL container starts for the first time.
+
+CREATE DATABASE IF NOT EXISTS seasoning_management_db;
+
+-- You can add more initialization commands here, for example:
+-- CREATE TABLE IF NOT EXISTS your_table_name (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(255) NOT NULL
+-- );
+
+-- GRANT ALL PRIVILEGES ON seasoning_management_db.* TO 'user'@'%';
+-- FLUSH PRIVILEGES;
