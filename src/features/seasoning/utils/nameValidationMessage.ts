631298c8b375739d0a4562114af3b936f4acb6cc
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2025-06-29 16:23:42 +0900

    ✨ [New Feature]: 調味料名バリデーションエラーメッセージ生成機能を追加
    - nameValidationErrorMessage関数: エラータイプから日本語メッセージを生成
    - seasoning機能専用の表示ロジック
    - 4つのエラータイプに対応するテストを追加

---
+++ src/features/seasoning/utils/nameValidationMessage.ts
@@ -0,0 +1,28 @@
+import type { NameValidationError } from "../../../utils/nameValidation";
+import {
+  VALIDATION_CONSTANTS,
+  VALIDATION_ERROR_MESSAGES,
+} from "../../../constants/validation";
+
+/**
+ * 調味料名バリデーションエラーのメッセージ表示
+ * seasoning機能専用の表示ロジック
+ */
+export const nameValidationErrorMessage = (
+  error: NameValidationError
+): string => {
+  switch (error) {
+    case "REQUIRED":
+      return VALIDATION_ERROR_MESSAGES.NAME_REQUIRED;
+    case "LENGTH_EXCEEDED":
+      return VALIDATION_ERROR_MESSAGES.NAME_LENGTH_EXCEEDED(
+        VALIDATION_CONSTANTS.NAME_MAX_LENGTH
+      );
+    case "INVALID_FORMAT":
+      return VALIDATION_ERROR_MESSAGES.NAME_INVALID_FORMAT;
+    case "NONE":
+      return "";
+    default:
+      return "";
+  }
+};
